95f431b9b98f649c49455bcd48be44c46849fb6e
NicoForslund <nicofors2015@gmail.com>
2019-08-22 10:18:09 +0300

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let MyButton = document.getElementById("button");
+
+let MyText = document.getElementById("Hello World");
+
+button.addEventListener("click", OnButtonClicked);
+
+function OnButtonClicked() {
+
+
+    alert("Hello World")
+};
